9e2d006c3e6fc358659bc07968dbd9b75fcc9f69
gregoryfryns <gregory.fryns@gmail.com>
2014-12-19 17:05:22 -0800

    Updated comments in cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,18 @@
 ## This file defines two functions to store matrices and their inverses to 
 ## a cache and retrieve them 
 
-## makeCacheMatrix creates a vector of functions that can
+## Example usage :
+#  > mat <- matrix(sample.int(16,16),4,4) # generates a random 4x4 matrix
+#  > matCache <- makeCacheMatrix(mat)     # makes the cached version of the matrix
+#  > cacheSolve(matCache)                 # returns the inverse of the matrix
+#                                         # (computes it and stores it in the cache)
+#  > cacheSolve(matCache)                 # returns the inverse of the matrix
+#                                         # (from the cache this time)
+
+
+## makeCacheMatrix creates a cached version of the matrix given as an input
+## This cached version can store the matrix's inverse. It generates a vector 
+## of functions that can
 ##   1. set the matrix value in the cache
 ##   2. get the matrix from the cache
 ##   3. set the inverse of the matrix in the cache
@@ -20,7 +31,7 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## cacheSolve returns the inverse of the matrix. 
+## cacheSolve returns the inverse of the cached matrix given as an argument. 
 ##   If the inverse is in the cache, this value is returned. 
 ##   If not the inverse is computed and stored in the cache
 ## This function assumes all the provided matrices are invertible
